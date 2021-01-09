b44eb732a6c86c4af26c290ac88d7511d50b2a22
hanaheyo <violets5618@gmail.com>
2021-01-10 00:03:54 +0900

    First step with TypeScript

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+const name = "Hana",
+    age = 24,
+    gender = 'female';
+
+const sayHi = (name, age, gender?) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`)
+}
+
+sayHi(name, age);
+
+export {}; // TypeScript에서는 이걸 꼭 해줘야 함. 위에 내용이 모듈이 된다는 뜻이야.
