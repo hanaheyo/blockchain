31568441bdfcebfba3fe5f4449bb1bcd18acdffd
hanaheyo <violets5618@gmail.com>
2021-01-10 16:19:06 +0900

    Classes on TypeScript

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const person = {
-    name: "Hana",
-    age: 22,
-    gender: "female"
-};
+class Human {
+    constructor(name, age, gender) {
+        this.name = name; // class의 name이 constructor의 name과 같다는 뜻.
+        this.age = age;
+        this.gender = gender;
+    }
+}
+const kyungmin = new Human("Kyungmin", 29, "male");
 const sayHi = (person) => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
 };
-console.log(sayHi(person));
+console.log(sayHi(kyungmin));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-interface Human {
-    name: string;
-    age: number;
-    gender: string;
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender?: string) {
+        this.name = name; // class의 name이 constructor의 name과 같다는 뜻.
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const person = {
-    name: "Hana",
-    age: 22,
-    gender: "female"
-}
+const kyungmin = new Human("Kyungmin", 29, "male");
 
 const sayHi = (person: Human):string => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`
 }
 
-console.log(sayHi(person));
+console.log(sayHi(kyungmin));
 
 export {}; // TypeScript에서는 이걸 꼭 해줘야 함. 위에 내용이 모듈이 된다는 뜻이야.
